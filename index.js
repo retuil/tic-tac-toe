8712645c55ce044a6b7f62a273f31c97379a2be6
Сурганов Денис <surganov.denis@gmail.com>
2025-02-25 16:58:33 +0500

    Работает, но символы могут убираться

--- index.js
+++ index.js
@@ -1,114 +1,10 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
-const DECIDE_WINNER = {CROSS: CROSS, ZERO: ZERO, EMPTY: 'Победила дружба.'};
-
-let counter = 0;
-let game = Board();
+//const DECIDE_WINNER = {CROSS: CROSS, ZERO: ZERO, EMPTY: };
 
 const container = document.getElementById('fieldWrapper');
 
-startGame();
-addResetListener();
-
-function startGame() {
-    renderGrid(3);
-}
-
-function renderGrid(dimension) {
-    container.innerHTML = '';
-
-    for (let i = 0; i < dimension; i++) {
-        const row = document.createElement('tr');
-        for (let j = 0; j < dimension; j++) {
-            const cell = document.createElement('td');
-            cell.textContent = EMPTY;
-            cell.addEventListener('click', () => cellClickHandler(i, j));
-            row.appendChild(cell);
-        }
-        container.appendChild(row);
-    }
-}
-
-function cellClickHandler(row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
-    let symbol;
-    if (game.get(row, col) === EMPTY) {
-        if (counter % 2 === 0) {
-            symbol = CROSS;
-        } else {
-            symbol = ZERO;
-        }
-        renderSymbolInCell(symbol, row, col);
-        game.set(symbol, row, col);
-        counter++;
-    }
-
-    let winner = game.checkWin();
-    if (winner !== null) {
-        alert(DECIDE_WINNER[winner]);
-    }
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
-    renderSymbolInCell(symbol, row, col);
-}
-
-function renderSymbolInCell(symbol, row, col, color = '#333') {
-    const targetCell = findCell(row, col);
-
-    targetCell.textContent = symbol;
-    targetCell.style.color = color;
-}
-
-function findCell(row, col) {
-    const targetRow = container.querySelectorAll('tr')[row];
-    return targetRow.querySelectorAll('td')[col];
-}
-
-function addResetListener() {
-    const resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', resetClickHandler);
-}
-
-function resetClickHandler() {
-    console.log('reset!');
-}
-
-
-/* Test Function */
-
-/* Победа первого игрока */
-function testWin() {
-    clickOnCell(0, 2);
-    clickOnCell(0, 0);
-    clickOnCell(2, 0);
-    clickOnCell(1, 1);
-    clickOnCell(2, 2);
-    clickOnCell(1, 2);
-    clickOnCell(2, 1);
-}
-
-/* Ничья */
-function testDraw() {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
-}
-
-function clickOnCell(row, col) {
-    findCell(row, col).click();
-}
 
 class Board {
     constructor() {
@@ -196,3 +92,120 @@ class Board {
         return null;
     }
 }
+
+
+let counter = 0;
+let game = new Board();
+
+
+startGame();
+addResetListener();
+
+function startGame() {
+    renderGrid(3);
+}
+
+function renderGrid(dimension) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < dimension; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = EMPTY;
+            cell.addEventListener('click', () => cellClickHandler(i, j));
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
+
+function cellClickHandler(row, col) {
+    // Пиши код тут
+    console.log(`Clicked on cell: ${row}, ${col}`);
+    let symbol;
+    if (game.get(row, col) === EMPTY) {
+        if (counter % 2 === 0) {
+            symbol = CROSS;
+        } else {
+            symbol = ZERO;
+        }
+        renderSymbolInCell(symbol, row, col);
+        game.set(symbol, row, col);
+        counter++;
+    }
+
+    let winner = game.checkWin();
+    if (winner !== null) {
+        let result;
+        if (winner === ' '){
+            result = 'Победила дружба.';
+        }
+        else {
+            result = winner
+        }
+
+        alert(result);
+    }
+
+
+    /* Пользоваться методом для размещения символа в клетке так:
+        renderSymbolInCell(ZERO, row, col);
+     */
+    renderSymbolInCell(symbol, row, col);
+}
+
+function renderSymbolInCell(symbol, row, col, color = '#333') {
+    const targetCell = findCell(row, col);
+
+    targetCell.textContent = symbol;
+    targetCell.style.color = color;
+}
+
+function findCell(row, col) {
+    const targetRow = container.querySelectorAll('tr')[row];
+    return targetRow.querySelectorAll('td')[col];
+}
+
+function addResetListener() {
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', resetClickHandler);
+}
+
+function resetClickHandler() {
+    console.log('reset!');
+}
+
+
+/* Test Function */
+
+/* Победа первого игрока */
+function testWin() {
+    clickOnCell(0, 2);
+    clickOnCell(0, 0);
+    clickOnCell(2, 0);
+    clickOnCell(1, 1);
+    clickOnCell(2, 2);
+    clickOnCell(1, 2);
+    clickOnCell(2, 1);
+}
+
+/* Ничья */
+function testDraw() {
+    clickOnCell(2, 0);
+    clickOnCell(1, 0);
+    clickOnCell(1, 1);
+    clickOnCell(0, 0);
+    clickOnCell(1, 2);
+    clickOnCell(1, 2);
+    clickOnCell(0, 2);
+    clickOnCell(0, 1);
+    clickOnCell(2, 1);
+    clickOnCell(2, 2);
+}
+
+function clickOnCell(row, col) {
+    findCell(row, col).click();
+}
+
+
